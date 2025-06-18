54971499960f24160deccbd04d2256e8e60dfa26
xponend <xponend@gmail.com>
2025-06-18 11:12:16 +0500

    password

--- EmployeeTrackingWeb/EmployeeTracking/settings.py
+++ EmployeeTrackingWeb/EmployeeTracking/settings.py
@@ -16,7 +16,8 @@ from django.db.backends.mysql.base import DatabaseWrapper
 
 from django.contrib.messages import constants as messages
 
-DatabaseWrapper.data_types["DateTimeField"] = "datetime"
+# Remove this line - it's causing the error
+# DatabaseWrapper.data_types["DateTimeField"] = "datetime"
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
